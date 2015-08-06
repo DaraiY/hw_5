5ed5ac4998ad9908a5b33ec3c9e92eb224e3fdef
Darai <daraiyoo@gmail.com>
2015-08-05 21:15:04 -0400

    arrays and types of data added in js

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,43 @@ var color ="gray";
 
 var x = 10;
 console.log(x + 30);
+
+
+//alert
+alert("Welcome!");
+
+
+
+//different data types
+"Hello World" //string
+2,3,4,-4,-74 //numbers
+true, false //boolean
+
+
+
+//arrays- used to hold a collection of date
+
+["blue", "red","green","yellow" ]; //full of strings
+
+[1,2,3,100,837,"kitty"]; //can hold multiple data types
+
+ var sizes=["small", "medium", "large"]; //arrays can be stored in variables
+
+
+
+ //multi-dimensional arrays
+ var birds=["pidgeon", "parrots"];
+ var reptiles=["snakes", "lizards"];
+ var animals = [birds, reptiles];
+
+ //access items nested in multi-dimensional arrays
+ var animals = [["pidgeons","parrots",],["snakes","lizards"]];
+ //access the first array inside then the first item inside that array
+ console.log(animals[0][0]);
+//access the second array inside then the first item inside that array
+console.log(animals[1][0]);
+
+
+
+
+
